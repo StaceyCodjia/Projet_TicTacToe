f78f5a53468cf9c08cc3e621e5786af7b552cfa0
Stacey <staceycodjia@gmail.com>
2024-12-03 09:57:23 +0100

    Menu Démarrer

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
-//jxjsjsjdjshushqh
+void modeDeuxJoueurs() {}
+void modeIA() {}
+
+int main()
+{
+    int choix;
+    do{
+        std::cout << "=====================\n";
+        std::cout << "   Jeu de Morpion    \n";
+        std::cout << "=====================\n";
+        std::cout << "1. Jouer à 2 joueurs\n";
+        std::cout << "2. Jouer contre l'IA\n";
+        std::cout << "3. Quitter\n";
+        std::cout << "Entrez votre choix : ";
+        std::cin >> choix;
+        switch (choix)
+        {
+        case 1:
+        modeDeuxJoueurs();
+        break;
+        case 2:
+        modeIA();
+        break;
+        case 3:
+        std::cout <<"Au revoir !"<<std::endl;
+        break;
+
+        default:
+        std::cout <<"Erreur ! Reessayer" <<std::endl;
+        break;
+        }
+    }
+    while(choix !=3);
+
+}
